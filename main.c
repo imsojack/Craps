c3c88c7e1065f5b902e954a37e7aacaf5b0109e8
imsojack <107078414+imsojack@users.noreply.github.com>
2022-07-03 15:44:02 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+* Programmer: Jack Balster
+* Class: CptS 121; Lab Section 4
+* Programming Assignment: Programming Assignment 4
+* Date: 2/19/2022
+* Description: This program contains functions that allows the user to play a game of craps.
+* It allows the user to place wagers and displays according messages.
+*/
+
+#include "header.h" 
+
+int main(void)
+{
+	play_game();
+	return 0;
+}
